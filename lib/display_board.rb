df890c289eb9c41c8cc2d7f48aa949b1044f9337
morra.dianna <morra.dianna@gmail.com>
2020-04-27 20:45:32 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -2,9 +2,9 @@ board = ["X", "X", "X", " ", "", " ", " ", " ", " "]
 x = "#{board}"
 o = "#{board}"
 def display_board
-  puts "   |   |   "
-  puts "-----------"
-  puts "   |   |   "
-  puts "-----------"
-  puts "   |   |   "
+ puts " #{board[0]} | #{board[1]} | #{board[2]} "
+ puts "-----------"
+ puts " #{board[0]} | #{board[1]} | #{board[2]} "
+ puts "-----------"
+ puts " #{board[0]} | #{board[1]} | #{board[2]} "
 end 
