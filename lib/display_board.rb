beb915efc286f01c418526b9910738a1c1fa085c
morra.dianna <morra.dianna@gmail.com>
2020-04-27 20:49:47 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -3,7 +3,7 @@ board[0] = "X"
 board[1] = "X"
 board[2] = "X"
 
-def display_board
+def display_board(board)
  puts " #{board[0]} | #{board[1]} | #{board[2]} "
  puts "-----------"
  puts " #{board[3]} | #{board[4]} | #{board[5]} "
